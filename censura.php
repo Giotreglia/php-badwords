63dd9b999bea39576c1ff072e3b629cac6d81821
Giotreglia <giovanni.treglia@gmail.com>
2023-04-17 15:59:24 +0200

    Programma finito e funzionante

---
+++ censura.php
@@ -0,0 +1,37 @@
+<?php
+    $paragrafo = $_GET['paragraph'];
+    $censura = $_GET['badword'];
+    $paragrafoCensurato = str_replace($censura, '*** ', $paragrafo); 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Badwords</title>
+</head>
+<body style="font-family:arial;">
+
+    <div style="max-width:1170px; margin:auto;">
+        <div>
+            <h1>Paragrafo Uncensored</h1>
+            <p>
+                "<?php echo $paragrafo; ?>"
+            </p>
+            <h4>Il paragrafo è lungo <?php echo strlen($paragrafo);?> caratteri</h4>
+        </div>
+
+        <div>
+            <h1>Paragrafo Censored</h1>
+            <p>
+                "<?php echo $paragrafoCensurato; ?>"
+            </p>
+            <h4>Il paragrafo è lungo <?php echo strlen($paragrafoCensurato);?> caratteri</h4>
+        </div>
+
+    </div>
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Badwords</title>
+</head>
+<body style="font-family:arial;">
+
+    <div style="max-width:1170px; margin:auto;">
+        <form method="GET" action="censura.php" style="display:flex; flex-direction:column; gap:20px;">
+            <label for="paragraph">Paragrafo</label>
+            <textarea name="paragraph" id="paragraph" cols="30" rows="10" placeholder="Inserisci il testo da censurare"></textarea>
+    
+            <label for="badword">Badword</label>
+            <input name="badword" id="badword" type="text" placeholder="Inserisci la parola da censurare">
+    
+            <button type="submit">Invia</button>
+        </form>
+    </div>
+    
+</body>
+</html>
